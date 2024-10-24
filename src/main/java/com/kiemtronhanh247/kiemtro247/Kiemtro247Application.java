7c713d7dffd0a36b5e1081df9348df8625147c3c
vanlong2202 <97232913+vanlong2202@users.noreply.github.com>
2024-10-24 14:22:19 +0700

    Initial commit

---
+++ src/main/java/com/kiemtronhanh247/kiemtro247/Kiemtro247Application.java
@@ -0,0 +1,13 @@
+package com.kiemtronhanh247.kiemtro247;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kiemtro247Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kiemtro247Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiemtronhanh247/kiemtro247/Kiemtro247ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiemtronhanh247.kiemtro247;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kiemtro247ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
